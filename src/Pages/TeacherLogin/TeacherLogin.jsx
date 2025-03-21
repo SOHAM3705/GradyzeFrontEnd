9168fba6fbd301c5476aed8edf681641e7a35f66
SOHAM3705 <sohampachpute05@gmail.com>
2025-03-21 21:52:52 +0530

    modified more

--- src/Pages/TeacherLogin/TeacherLogin.jsx
+++ src/Pages/TeacherLogin/TeacherLogin.jsx
@@ -27,6 +27,7 @@ const TeacherLogin = () => {
         { email, password }
       );
 
+      localStorage.setItem("token", response.data.token);
       localStorage.setItem("adminId", response.data.adminId); // Store adminId
       localStorage.setItem("teacherId", response.data.teacher._id); // Store teacherId
       localStorage.setItem("teacherName", response.data.teacher.name); // Store teacherName
