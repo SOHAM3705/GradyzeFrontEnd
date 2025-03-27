60fd8269ede9a183834a9767b6bb451a887eecfb
SOHAM3705 <sohampachpute05@gmail.com>
2025-03-27 16:43:30 +0530

    admin reset password

--- src/Pages/ForgetPassword/ChangePassword.jsx
+++ src/Pages/ForgetPassword/ChangePassword.jsx
@@ -60,7 +60,12 @@ const ChangePassword = () => {
 
       const response = await axios.post(
         "https://gradyzebackend.onrender.com/api/password/change-password",
-        { token, newPassword, confirmPassword } // Ensure token is in the body
+        { token, newPassword, confirmPassword },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`, // ✅ Add this
+          },
+        }
       );
 
       console.log("✅ Response:", response.data);
