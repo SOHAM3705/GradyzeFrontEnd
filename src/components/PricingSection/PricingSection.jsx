9a510977f429a0dd9b4251c509b3be5b94f568f1
SOHAM3705 <sohampachpute05@gmail.com>
2025-05-01 09:43:15 +0530

    Subscription plan changes

--- src/components/PricingSection/PricingSection.jsx
+++ src/components/PricingSection/PricingSection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FaCheckCircle, FaStar, FaTrophy, FaBuilding } from "react-icons/fa";
 import "./SubscriptionPlan.css";
+import { Link } from "react-router-dom";
 
 const SubscriptionPlan = () => {
   const plans = [
