d1024e6349bd625e1647b1d6a38639e73991dc3b
SOHAM3705 <sohampachpute05@gmail.com>
2025-06-05 00:28:55 +0530

    ui changes

--- src/components/TeacherDashMenu/Forms/TestClassSelector.jsx
+++ src/components/TeacherDashMenu/Forms/TestClassSelector.jsx
@@ -102,12 +102,6 @@ const TestClassSelector = () => {
               {teacherData.assignedClass.section &&
                 ` (${teacherData.assignedClass.section})`}
             </p>
-            <button
-              className="mt-2 bg-blue-500 text-white px-4 py-2 rounded-lg"
-              onClick={() => setSelectedClass(teacherData.assignedClass)}
-            >
-              Select This Class
-            </button>
           </div>
         </div>
       )}
