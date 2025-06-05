59d8dd2915be1a55a696a8a3bcf14530e9e3e80d
SOHAM3705 <sohampachpute05@gmail.com>
2025-06-05 19:39:31 +0530

    update test

--- src/utils/tests.jsx
+++ src/utils/tests.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { API_BASE_URL } from "../../config";
+import { API_BASE_URL } from "../config";
 
 const TestPage = () => {
   const { id } = useParams();
